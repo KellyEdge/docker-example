f1d64a99df885b16885c4fcb0d9ad4d570399085
wes <westly.edge@gmail.com>
2021-04-27 18:01:31 -0500

    ecs is hosting the container on a single ec2

---
+++ iac/scripts/create-stack.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+REGION=us-east-2
+
+aws --region $REGION cloudformation create-stack \
+--stack-name "kelly-ecs-stack" \
+--template-body "file://$SCRIPT_DIR/../template.yaml" \
+--capabilities "CAPABILITY_NAMED_IAM" \
+--parameters \
+ParameterKey=ClusterName,ParameterValue="kelly-ecs-cluster" \
+ParameterKey=EC2ImageId,ParameterValue="ami-06e650acd294da294" \
+ParameterKey=EC2InstanceType,ParameterValue="t2.medium" \
+ParameterKey=ServiceName,ParameterValue="kelly-ecs-service" \
+ParameterKey=RoleName,ParameterValue="kelly-ecs-role" \
+ParameterKey=KeyPairName,ParameterValue="wes-keypair" \
+ParameterKey=ContainerImage,ParameterValue="wesedge/web-app:latest"
---
+++ iac/scripts/ssh-to-ec2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh -i "~/.ssh/wes-keypair.pem" ec2-user@ec2-18-217-16-203.us-east-2.compute.amazonaws.com
