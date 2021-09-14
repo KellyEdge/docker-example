713b1fbbabf61dca3627bb9610a6e54424c2e2a4
Kelly Edge <kellyedge@kellys-air-gold.lan>
2021-09-14 16:49:08 -0500

    ...

--- iac/scripts/ssh-to-ec2.sh
+++ iac/scripts/ssh-to-ec2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -i "~/.ssh/kelly-keypair.pem" ec2-user@ec2-18-217-16-203.us-east-2.compute.amazonaws.com
+ssh -i "~/.ssh/kelly-keypair.pem" ec2-user@ec2-18-117-171-119.us-east-2.compute.amazonaws.com
