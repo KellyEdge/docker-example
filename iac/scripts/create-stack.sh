16af16d2ac1c37261fadab5ff2b0544d013d1191
kellyedge <edge.kelly@gmail.com>
2021-11-09 18:24:58 -0600

    Add input parameters for template.yaml

--- iac/scripts/create-stack.sh
+++ iac/scripts/create-stack.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 SCRIPT_DIR=$(dirname "$0")
 REGION=us-east-2
 
@@ -10,10 +9,20 @@ aws --region $REGION cloudformation create-stack \
 --capabilities "CAPABILITY_NAMED_IAM" \
 --parameters \
 ParameterKey=ClusterName,ParameterValue="kelly-ecs-cluster" \
-ParameterKey=EC2ImageId,ParameterValue="ami-06e650acd294da294" \
+ParameterKey=EC2ImageId,ParameterValue="ami-0102692edc680b5b0" \
 ParameterKey=EC2InstanceType,ParameterValue="t2.medium" \
 ParameterKey=ServiceName,ParameterValue="kelly-ecs-service" \
 ParameterKey=RoleName,ParameterValue="kelly-ecs-role" \
 ParameterKey=KeyPairName,ParameterValue="kelly-keypair" \
 ParameterKey=ContainerImage,ParameterValue="kellyedge/web-app:latest" \
-ParameterKey=LaunchConfigurationName,ParameterValue="kelly-launch-config1"
+ParameterKey=LaunchConfigurationName,ParameterValue="kelly-launch-config1" \
+ParameterKey=SubnetIds,ParameterValue="subnet-8852e0e3\, subnet-ef2b78a3\, subnet-c87b93b5" \
+ParameterKey=AutoScalingGroupMinSize,ParameterValue="2" \
+ParameterKey=AutoScalingGroupMaxSize,ParameterValue="2" \
+ParameterKey=VpcId,ParameterValue="vpc-9fb339f4" \
+ParameterKey=ContainerServiceDesiredCount,ParameterValue="1" \
+ParameterKey=ALBListenerPort,ParameterValue="80" \
+ParameterKey=ALBListenerProtocol,ParameterValue="HTTP" \
+ParameterKey=ECSHostPort,ParameterValue="80" \
+ParameterKey=ECSContainerPort,ParameterValue="80" \
+ParameterKey=ECSTaskMemory,ParameterValue="512"
--- iac/scripts/update-stack.sh
+++ iac/scripts/update-stack.sh
@@ -9,11 +9,21 @@ aws --region $REGION cloudformation update-stack \
 --capabilities "CAPABILITY_NAMED_IAM" \
 --parameters \
 ParameterKey=ClusterName,ParameterValue="kelly-ecs-cluster" \
-ParameterKey=EC2ImageId,ParameterValue="ami-06e650acd294da294" \
+ParameterKey=EC2ImageId,ParameterValue="ami-0102692edc680b5b0" \
 ParameterKey=EC2InstanceType,ParameterValue="t2.medium" \
 ParameterKey=ServiceName,ParameterValue="kelly-ecs-service" \
 ParameterKey=RoleName,ParameterValue="kelly-ecs-role" \
 ParameterKey=KeyPairName,ParameterValue="kelly-keypair" \
 ParameterKey=ContainerImage,ParameterValue="kellyedge/web-app:latest" \
-ParameterKey=LaunchConfigurationName,ParameterValue="kelly-launch-config1"
+ParameterKey=LaunchConfigurationName,ParameterValue="kelly-launch-config1" \
+ParameterKey=SubnetIds,ParameterValue="subnet-8852e0e3\, subnet-ef2b78a3\, subnet-c87b93b5" \
+ParameterKey=AutoScalingGroupMinSize,ParameterValue="2" \
+ParameterKey=AutoScalingGroupMaxSize,ParameterValue="2" \
+ParameterKey=VpcId,ParameterValue="vpc-9fb339f4" \
+ParameterKey=ContainerServiceDesiredCount,ParameterValue="1" \
+ParameterKey=ALBListenerPort,ParameterValue="80" \
+ParameterKey=ALBListenerProtocol,ParameterValue="HTTP" \
+ParameterKey=ECSHostPort,ParameterValue="80" \
+ParameterKey=ECSContainerPort,ParameterValue="80" \
+ParameterKey=ECSTaskMemory,ParameterValue="512"
 
